1106e4c1c82978b498d795c8ce48d5f2f348db92
Nico2K7 <44556019+Nico2K7@users.noreply.github.com>
2018-12-31 21:57:32 +0200

    included dry questions for part 2

--- answers.py
+++ answers.py
@@ -48,28 +48,32 @@ def part2_dropout_hp():
 
 
 part2_q1 = r"""
-**Your answer:**
+No dropout had lower loss on training and testing and had a better acuracy on training but did worse on testing. That make sense since it overfits more that it will have higher certainty and lower generalization. it remmbers more then it actually trains.
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The low dropout vs high dropout is actually the more surprising one. the loss of 0.4 was much higher then 0.8 (both higher then no dropout). 0.4 trains less well then the 0.8 (expected it to surpass it since overfits more) and 0.8 does better then 0.4 on testing (make sense because of better generalization probably). so this answer was the more surprising one.
 
 """
 
 part2_q2 = r"""
-**Your answer:**
+
+**Yes. It is possibole. Example:
+
+1 epoch for example has 10 examples with a binary result:
+
+the first got 8/10 right
+
+with probability almost 1 for correct answers and probabiliy almost 0.5 for wrong answers. therefore a low loss of about 1.
+
+The second got 9/10 right
+
+with probability of almost 0.5 for all the answers so the loss is much higer and around 6.
+
+So in this this example the loss has increase but also the accuracy.**
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+
+
+
 
 """
 # ==============
